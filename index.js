01632548aa2ae2e086a16b13933917477db5450a
Yoschihiro Kaimoto <116176473+hirokaimoto@users.noreply.github.com>
2022-10-30 15:46:44 -0300

    adjusted Js (input length)

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-// let passwordOne = "";
-// let passwordTwo = "";
-let firstPw = document.querySelector("#fieldone-el");
-let secondPw = document.querySelector("#fieldtwo-el");
 
+const firstPw = document.querySelector("#fieldone-el");
+const secondPw = document.querySelector("#fieldtwo-el");
+let pwSize = prompt('Insert the desired password length, Standard length is 15');
+//pwSize = Number(pwSize);
+console.log(pwSize);
 function generatePassword () {
     let passwordOne = "";
     let passwordTwo = "";
-    for (let i = 0; i < 15; i++){
-        let randomCharOne = Math.floor(Math.random() * characters.length);
-        let randomCharTwo = Math.floor(Math.random() * characters.length);
-        passwordOne += characters[randomCharOne];
-        passwordTwo += characters[randomCharTwo]; 
+
+    if (isNaN(pwSize) || pwSize === "") {
+        for (let i = 0; i < 15; i++){
+            let randomCharOne = Math.floor(Math.random() * characters.length);
+            let randomCharTwo = Math.floor(Math.random() * characters.length);
+            passwordOne += characters[randomCharOne];
+            passwordTwo += characters[randomCharTwo]; 
+        }
     }
-    firstPw.textContent = passwordOne;
-    secondPw.textContent = passwordTwo;
+    else {
+        for (let i = 0; i < pwSize; i++){
+            let randomCharOne = Math.floor(Math.random() * characters.length);
+            let randomCharTwo = Math.floor(Math.random() * characters.length);
+            passwordOne += characters[randomCharOne];
+            passwordTwo += characters[randomCharTwo]; 
+        }
+}
+    firstPw.value = passwordOne;
+    secondPw.value = passwordTwo;
 }
